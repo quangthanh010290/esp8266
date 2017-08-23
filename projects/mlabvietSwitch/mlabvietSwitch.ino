39dd1b7d05fd51cd894d81720413aeeefcf56397
quangthanh010290 <thanh@autonomous.nyc>
2017-08-23 23:23:35 +0700

    upate

--- projects/mlabvietSwitch/mlabvietSwitch.ino
+++ projects/mlabvietSwitch/mlabvietSwitch.ino
@@ -3,10 +3,12 @@
 MlabSwitch mSwitch;
 void setup() {
   // put your setup code here, to run once:
-	mSwitch.Init();
+	mSwitch.Init("iotdev-6b58b.firebaseio.com","5zg8yd7bazlRj0heqBZvrduyyjykvshtRPCbjlYh");
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+	mSwitch.Run();
 
 }
